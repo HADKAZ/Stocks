c9140577e232d7b268a9e136522b64e877629a24
ElZacho200a <zacheedescamps1@gmail.com>
2025-07-14 12:04:09 +0200

    Revert "feat(srvc-inventory):"added deleteBeer enpoints""
    This reverts commit 1c49e46e

--- repo-inventory/src/main/java/fr/lateb/domain/service/BeerService.java
+++ repo-inventory/src/main/java/fr/lateb/domain/service/BeerService.java
@@ -1,7 +1,5 @@
 package fr.lateb.domain.service;
 
-import fr.lateb.api.request.AddBeerRequest;
-import fr.lateb.api.request.GetBeerByRequest;
 import fr.lateb.api.response.GetBeerResponse;
 import fr.lateb.converter.BeerConverter;
 import fr.lateb.converter.BeerFormatConverter;
@@ -10,20 +8,17 @@ import fr.lateb.converter.BreweryConverter;
 import fr.lateb.data.model.BeerFormatModel;
 import fr.lateb.data.model.BeerModel;
 import fr.lateb.data.model.BeerTypeModel;
-import fr.lateb.data.model.BreweryModel;
 import fr.lateb.data.repository.BeerFormatRepository;
 import fr.lateb.data.repository.BeerRepository;
 import fr.lateb.data.repository.BeerTypeRepository;
 import fr.lateb.data.repository.BreweryRepository;
 import fr.lateb.domain.entity.BeerEntity;
-import fr.lateb.domain.entity.BreweryEntity;
 import fr.lateb.errors.ErrorsCode;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 
 import java.util.List;
-import java.util.UUID;
 
 @ApplicationScoped
 public class BeerService {
@@ -36,8 +31,6 @@ public class BeerService {
     BreweryRepository breweryRepository;
     @Inject
     BeerFormatRepository beerFormatRepository;
-
-
     @Transactional
     public void registerBeer(Long barcode, String name, String description, Float alcoholPercentage, BeerTypeModel beerTypeModel, BeerFormatModel beerFormatModel) {
 
@@ -57,31 +50,30 @@ public class BeerService {
     }
 
     @Transactional
-    public GetBeerResponse registerBeer(AddBeerRequest beer) {
+    public GetBeerResponse registerBeer(BeerEntity beer) {
 
 
-        var beerModel = beerRepository.findById(Long.valueOf(beer.barcode()));
+        var beerModel = beerRepository.findById(beer.id());
         if (beerModel != null)
             ErrorsCode.BEER_ALREADY_EXISTS.throwException(beerModel.name);
 
-        var newBeer =  new BeerModel();
-        newBeer.setName( beer.name());
-        newBeer.setId(Long.valueOf(beer.barcode()));
-        newBeer.format
+        var newBeer = BeerConverter.toModel(beer);
+        var brewery = newBeer.getBrewery();
+        var format = newBeer.getFormat();
+        var type = newBeer.getType();
 
 
+        if(breweryRepository.find("name",beer.brewery().name()).firstResult() == null) // To prevent transient error
+            breweryRepository.persist(brewery);
+        brewery = breweryRepository.find("name",beer.brewery().name()).firstResult();
 
-        if(breweryRepository.find("name",beer.brewery()).firstResult() == null) // To prevent transient error
-            breweryRepository.persist(BreweryConverter.toModel(new BreweryEntity(beer.brewery(), "","" )));
-        var brewery = breweryRepository.find("name",beer.brewery()).firstResult();
-
-        if(beerFormatRepository.find("name",beer.format()).firstResult() == null)// To prevent transient error
+        if(beerFormatRepository.find("name",beer.format().name()).firstResult() == null)// To prevent transient error
             beerFormatRepository.persist(format);
-        var format = beerFormatRepository.find("name",beer.format().name()).firstResult();
+        format = beerFormatRepository.find("name",beer.format().name()).firstResult();
 
         if(beerTypeRepository.find("type",beer.type().type()).firstResult() == null)// To prevent transient error
             beerTypeRepository.persist(type);
-        var type = beerTypeRepository.find("type",beer.type().type()).firstResult();
+        type = beerTypeRepository.find("type",beer.type().type()).firstResult();
 
         newBeer.setBrewery(brewery);
         newBeer.setFormat(format);
@@ -105,18 +97,7 @@ public class BeerService {
         return beerRepository.findAll().stream().map(GetBeerResponse::fromModel).toList();
     }
 
-    @Transactional
-    public List<GetBeerResponse> getBeersBy(String field , String value) {
-        try{
-           return beerRepository.find(field , value).stream().map(GetBeerResponse::fromModel).toList();
-        }catch(Exception e){
-            ErrorsCode.INVALID_FIELD.throwException(field);
-        }
-        return null;
-    }
-
     public BeerEntity getBeerById(Long barcode) {
         return BeerConverter.toEntity(beerRepository.findById(barcode));
     }
-
 }
